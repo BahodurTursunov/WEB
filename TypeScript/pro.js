ae40dce5b493ab930471db8ce7f83fc32f39443a
Bahodurkhon <tursunovb18@gmail.com>
2025-03-12 22:50:40 +0500

    немного про типы данных

--- TypeScript/pro.js
+++ TypeScript/pro.js
@@ -1,3 +1,7 @@
 var firstName = "Tom";
 var age = 25;
 var sentence = "Name ".concat(firstName, "\n    Age:").concat(age);
+var someVar = "hello";
+console.log(someVar);
+someVar = 20;
+console.log(someVar);
--- TypeScript/pro.ts
+++ TypeScript/pro.ts
@@ -1,5 +1,10 @@
-  
-  let firstName:string="Tom";
-  let age:number=25;
-    let sentence:string=`Name ${firstName}
+
+let firstName: string = "Tom";
+let age: number = 25;
+let sentence: string = `Name ${firstName}
     Age:${age}`;
+
+let someVar: any = "hello";
+console.log(someVar);
+someVar = 20;
+console.log(someVar);
