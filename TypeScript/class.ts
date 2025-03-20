7d9e3a423ea5bba5d4f945abfaf5d116372a617b
b_tursunov <b_tursunov@01-B-TUSUNOV>
2025-03-20 15:20:48 +0500

    закоментирован весь код, дабы не мешало другим файлам

--- TypeScript/class.ts
+++ TypeScript/class.ts
@@ -1,10 +1,11 @@
+/*
 class Users{
     readonly name: string = "Bakha";
-    a: number = 0;/*
+    a: number = 0;/!*
     constructor(name:string,age:number){
         this.n = name;
         this.a = age;
-    }*/
+    }*!/
     Print(){
         console.log(`Name: ${this.name}`);
     }
@@ -16,3 +17,4 @@ let t = new Users();
 t.a = 1;
 console.log(`name: ${t.name} age: ${t.a}`);
 
+*/
