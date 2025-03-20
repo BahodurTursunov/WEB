0e6ab7c24e1e3c636f0531ef784c001ba5e8ec72
b_tursunov <b_tursunov@01-B-TUSUNOV>
2025-03-20 14:49:37 +0500

    разобрана тема по классам

---
+++ TypeScript/class.ts
@@ -0,0 +1,18 @@
+class Users{
+    readonly name: string = "Bakha";
+    a: number = 0;/*
+    constructor(name:string,age:number){
+        this.n = name;
+        this.a = age;
+    }*/
+    Print(){
+        console.log(`Name: ${this.name}`);
+    }
+}
+
+// let t = new Users("John", 21);
+let t = new Users();
+
+t.a = 1;
+console.log(`name: ${t.name} age: ${t.a}`);
+
